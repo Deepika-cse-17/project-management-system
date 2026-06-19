d308965b0e9c4748aab7de4687329cd820598542
Deepika-cse-17 <deepikasabarinathan@gmail.com>
2026-06-19 16:37:31 +0530

    Update backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:5000/api' });
+const api = axios.create({ baseURL:'https://project-management-system-4862.onrender.com' });
 
 // Automatically attach JWT token to every request
 api.interceptors.request.use(config => {
